d3f415e11081176da9bc2cf588492b5ff0235abf
hundeva <hundeva@gmail.com>
2018-11-30 10:26:54 +0100

    add url launcher for external browser

--- lib/ui/webview_screen.dart
+++ lib/ui/webview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_times/app/strings.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatelessWidget {
@@ -16,6 +17,15 @@ class WebViewScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppStrings.of(context).articleReader),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.open_in_browser,
+              color: Colors.white,
+            ),
+            onPressed: () => launch(url),
+          ),
+        ],
       ),
       body: WebView(
         initialUrl: url,
